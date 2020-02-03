09d6e4cf16949d6a92d60c2388dc5e2748dc5d94
dueka <ekanemd@live.com>
2020-02-03 11:58:11 +0100

    created the login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,3 +1,22 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const Users = require("../users/users-model");
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.getBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!`, token });
+      } else {
+        res.status(401).json({ message: `Incorrect Credentials` });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 module.exports = router;
