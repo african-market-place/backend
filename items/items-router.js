0111e556e1067756f08007e504e5c31aeb0ccd2e
dueka <ekanemd@live.com>
2020-02-03 14:10:08 +0100

    created the items-model and items-router

--- items/items-router.js
+++ items/items-router.js
@@ -1,3 +1,36 @@
 const router = require("express").Router();
+const Products = require("./items-model");
+
+router.get("/", (req, res) => {
+  Products.getProducts()
+    .then(products => {
+      res.status(201).json(products);
+    })
+    .catch(error => {
+      res.status(500).json(err);
+    });
+});
+
+router.post("/addProducts", (req, res) => {
+  Products.addProducts(req.body)
+    .then(products => {
+      res.status(201).json(products);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Products.deleteProduct(id)
+    .then(deletedProduct => {
+      res.status(200).json({ message: "Product deleted" });
+    })
+    .catch(error => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
